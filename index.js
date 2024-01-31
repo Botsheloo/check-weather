b6ddf2c3c2d5ff7037921f680c565338bfa0f894
Botshelo <botshel89@gmail.com>
2024-01-31 21:03:42 +0200

    Updated search engine with API

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+function refreshWeather(response){
+  let temperature = document.querySelector(".temperature");
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = response.data.city;
+
+}
+function searchCity(city){
+  //make api call and update the interface
+  let apiKey = "o2e4e95f318btbd0334afc45b9074ea8";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
 function citySearch(event){
     event.preventDefault();
-    let h1 = document.querySelector("h1");
     let searchEngine = document.querySelector(".search-form-input");
-    h1.innerHTML = searchEngine.value;
+    searchCity(searchEngine.value);
 }
 let searchForm = document.querySelector(".search-form");
 searchForm.addEventListener("submit", citySearch);
